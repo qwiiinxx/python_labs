146a95f71320ae03623912afeb83ae7535db6ddc
qwiiinxx <aleksandrakabanova224@gmail.com>
2025-09-25 13:40:01 +0300

    new

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,10 +1,12 @@
 name = input().strip()
+name = ' '.join(name.split())
 name2 = name.split()
 
+
+tab_counter = name.count(' ')
 initials = ''.join(word[0].upper()for word in name2) + '.'
+
 
 print(f"ФИО: {name}")
 print(f"Инициалы: {initials}")
 print(f"Длина: {len(name)}")
-
-
---
+++ src/lab02/arrays.py
@@ -0,0 +1,26 @@
+def min_max(nums): # [3, -1, 5, 5, 0]
+    if not nums:
+        raise ValueError('Список пуст')
+    return (min(nums), max(nums))
+
+
+# print(min_max([3, -1, 5, 5, 0]))
+# print(min_max([42]))                 
+# print(min_max([-5, -2, -9]))         
+# print(min_max([]))                 
+# print(min_max([1.5, 2, 2.0, -3.1]))
+
+def unique_sorted(nums):
+    if nums == []:
+        return []
+    else:
+        return sorted(set(nums))
+    
+# print(unique_sorted([3, 1, 2, 1, 3]))
+# print(unique_sorted([]))
+# print(unique_sorted([-1, -1, 0, 2, 2]))
+# print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
+
+def flatten():
+    
