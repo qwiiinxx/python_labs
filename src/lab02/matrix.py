5fa9c81b553943ddadd4c907453c59cc806bdc7b
qwiiinxx <aleksandrakabanova224@gmail.com>
2025-09-26 16:11:05 +0300

    new

--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -36,7 +36,35 @@ def row_sums(nums):
 
     return result
 
-print(row_sums([[1, 2, 3], [4, 5, 6]]))
-print(row_sums([[-1, 1], [10, -10]]))
-print(row_sums([[0, 0], [0, 0]]))
-print(row_sums([[1, 2], [3]]))
+# print(row_sums([[1, 2, 3], [4, 5, 6]]))
+# print(row_sums([[-1, 1], [10, -10]]))
+# print(row_sums([[0, 0], [0, 0]]))
+# print(row_sums([[1, 2], [3]]))
+
+def col_sums(nums):
+    n_len = len(nums[0])
+    for n in nums:
+        if len(n) != n_len:
+            raise ValueError('рваная')
+    
+    result = []
+    for i in range(len(nums)-1): # [[1, 2, 3], [4, 5, 6]] -> [5, 7, 9]
+        if len(nums[i]) == 3:
+            sum1 = nums[i][0] + nums[i+1][0]
+            result.append(sum1)
+            sum2 = nums[i][1] + nums[i+1][1]
+            result.append(sum2)
+            sum3 = nums[i][2] + nums[i+1][2]
+            result.append(sum3)
+        else:                                # [[-1, 1], [10, -10]] -> [9, -9]
+            sum1 = nums[i][0] + nums[i+1][0]
+            result.append(sum1)
+            sum2 = nums[i][1] + nums[i+1][1]
+            result.append(sum2)
+
+    return result
+
+print(col_sums([[1, 2, 3], [4, 5, 6]])) # [5, 7, 9]
+print(col_sums([[-1, 1], [10, -10]])) # [9, -9]
+print(col_sums([[0, 0], [0, 0]])) # [0, 0]
+print(col_sums([[1, 2], [3]]))
