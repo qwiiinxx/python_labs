594009b8e88a7c4cf945be18692d12aa003c44ad
qwiiinxx <aleksandrakabanova224@gmail.com>
2025-09-27 16:30:54 +0300

    new

--- src/lab02/tuples.py
+++ src/lab02/tuples.py
@@ -1,5 +1,36 @@
-def format_record(rec): # ','.join()
-    result = []
-    for i in range(len(rec)):
-        if rec[i] is str:
-                        
+def format_record(rec):                     # ','.join()
+    fio, group, gpa = rec
+
+    if not isinstance(rec, tuple) or len(rec) != 3:
+        raise ValueError("нужен кортеж из 3-х элементов")
+
+    if not isinstance(fio, str) or not isinstance(group, str):
+        raise TypeError("данные должны быть строкой")
+    
+    if not fio:
+        raise ValueError("нельзя оставлять пустым")
+    
+    group = group.strip()
+    if not group:
+        raise ValueError("нельзя оставлять пустым")
+    
+    if not gpa:
+        raise ValueError("нельзя оставлять пустым")
+    
+    if not isinstance(gpa, float):
+        raise TypeError("данные должны быть вещественным числом")
+    
+    parts = fio.strip().split()
+    if len(parts) < 2:
+        raise ValueError("должны быть хотя бы фамилия и имя")
+    
+
+    surname = parts[0].title()
+    initials = "".join(p[0].upper() + '.' for p in parts[1:])
+
+    
+    
+    return parts, group, gpa
+    
+
+print(format_record("    иванов иван иванович   ", "BIVT-25", 4.6))
