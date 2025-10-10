d0f21dca4c8bf5257572d7c1b1dd9c4a0f4013ca
Aleksandra <Sasha.kabanova.777@mail.ru>
2025-10-10 15:51:15 +0300

    ПОСЛЕДНЕЕ Я НАДЕЮСЬ

--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -39,11 +39,15 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
             raise ValueError('рваная')
     
     result = []
-    for i, e in zip(mat[0], mat[1]):
-        result.append(i + e)
+    for i in range(n_len):
+        col_sum = 0    
+        for j in range(len(mat)):
+            col_sum += mat[j][i]
+        result.append(col_sum)
     return result
 
 print(col_sums([[1, 2, 3], [4, 5, 6]])) # [5, 7, 9]
+print(col_sums([[1, 3, 3], [1, 1, 1], [1, 1, 1]]))
 print(col_sums([[-1, 1], [10, -10]])) # [9, -9]
 print(col_sums([[0, 0], [0, 0]])) # [0, 0]
 print(col_sums([[1, 2], [3]]))
